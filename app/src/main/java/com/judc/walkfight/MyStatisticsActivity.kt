1a9355585f908a8936d6a84705bf8cf0279e75bf
Caetan <caetan@MacBook-Pro-de-Caetan.local>
2024-03-09 14:45:02 +0100

    Added toast and logs on InitMenu and MyStatistics activities

--- app/src/main/java/com/judc/walkfight/MyStatisticsActivity.kt
+++ app/src/main/java/com/judc/walkfight/MyStatisticsActivity.kt
@@ -2,7 +2,9 @@ package com.judc.walkfight
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MyStatisticsActivity : ComponentActivity() {
@@ -10,11 +12,25 @@ class MyStatisticsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.mystats)
+
+        val globalRankingButton: Button = findViewById(R.id.globalrankingmenu)
+        globalRankingButton.setOnClickListener {
+            val intentRankingActivity = Intent(this@MyStatisticsActivity, Ranking::class.java)
+            startActivity(intentRankingActivity)
+            Toast.makeText(this, "Going to Ranking", Toast.LENGTH_LONG).show()
+            Log.d(MyStatisticsActivity.TAG, "My Statistics Activity: Intent load Ranking")
+        }
         val myStatisticsButton: Button = findViewById(R.id.mainmenu)
         myStatisticsButton.setOnClickListener {
-            val intent = Intent(this@MyStatisticsActivity, InitMenuActivity::class.java)
-            startActivity(intent)
+            val intentMyStatisticsActivity = Intent(this@MyStatisticsActivity, InitMenuActivity::class.java)
+            startActivity(intentMyStatisticsActivity)
+            Toast.makeText(this, "Going to InitMenuActivity", Toast.LENGTH_LONG).show()
+            Log.d(MyStatisticsActivity.TAG, "My Statistics Activity: Intent load Init Menu Activity")
         }
 
+    }
+
+    companion object {
+        private const val TAG = "MyStatisticsActivity"
     }
 }
